e3130da7b5eca067a970224d69a11ef552336c14
mliooh <navillee88@gmai.com>
2024-12-23 10:27:51 +0300

    dummy data generation

--- book fakeit.go
+++ book fakeit.go
@@ -1,48 +1,44 @@
-package bookstore_api
+package main
 
-import (
-	"fmt"
-	"github.com/brianvoe/gofakeit/v6"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"math/rand"
-)
-
-// define book model
-
-type Book struct {
-	ID       uint   `gorm:"primary_key"`
-	Title    string `json:"title" validate:"required"`
-	Author   string `json:"author" validate:"required"`
-	Genre    string `json:"genre"`
-	Quantity uint   `json:"quantity"`
-}
-
-// initialise database
-var db *gorm.DB
-
-func loadDatabase() {
-	var db, err = gorm.Open(sqlite.Open("bookstore.db"), &gorm.Config{})
-	if err != nil {
-		fmt.Printf("failed to connect database")
-	}
-
-	// Create tables
-	db.AutoMigrate(&Book{})
-
-	//	gofakeit create data
-
-	var genres = []string{"Fiction", "Non-fiction", "Fantasy", "Sci-Fi", "Romance", "Thriller"}
-
-	for i := 0; i < 200; i++ {
-		book := Book{
-
-			Title:    gofakeit.BookTitle(),
-			Author:   gofakeit.Name(),
-			Genre:    genres[rand.Intn(len(genres))],
-			Quantity: uint(rand.Intn(50) + 1),
-		}
-		db.Create(&book)
-	}
-	println(fmt.Sprintf("Bookstore loaded %d books", 200))
-}
+//
+//import (
+//	"fmt"
+//	"github.com/brianvoe/gofakeit/v6"
+//	"gorm.io/driver/sqlite"
+//	"gorm.io/gorm"
+//	"math/rand"
+//)
+//
+//// initialise database
+//var db *gorm.DB
+//
+//func loadDatabase() {
+//	var err error
+//	db, err = gorm.Open(sqlite.Open("bookstore.db"), &gorm.Config{})
+//	if err != nil {
+//		fmt.Printf("failed to connect database")
+//	}
+//
+//	// Create tables
+//	db.AutoMigrate(&Book{})
+//	println("Database and tables created")
+//
+//}
+//
+//func createBooks() {
+//	//	gofakeit create data
+//
+//	var genres = []string{"Fiction", "Non-fiction", "Fantasy", "Sci-Fi", "Romance", "Thriller"}
+//
+//	for i := 0; i < 200; i++ {
+//		book := Book{
+//
+//			Title:    gofakeit.BookTitle(),
+//			Author:   gofakeit.Name(),
+//			Genre:    genres[rand.Intn(len(genres))],
+//			Quantity: uint(rand.Intn(50) + 1),
+//		}
+//		db.Create(&book)
+//	}
+//	println(fmt.Sprintf("Bookstore loaded %d books", 200))
+//}
--- main.go
+++ main.go
@@ -1,10 +1,73 @@
-package bookstore_api
+package main
 
 import (
+	"fmt"
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+
+	"gorm.io/gorm"
+	"math/rand"
 	"net/http"
-	_ "net/http"
 )
+
+type Book struct {
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	Author   string `json:"author"`
+	Title    string `json:"title"`
+	Genre    string `json:"genre"`
+	Quantity int    `json:"quantity"`
+}
+
+// initialise database
+var db *gorm.DB
+
+func loadDatabase() {
+
+	var err error
+	db, err = gorm.Open(sqlite.Open("file:bookstore.db?cache=shared&_fk=1"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Book{})
+	fmt.Println("Database and tables created")
+
+	/*var db, err = gorm.Open(sqlite.Open("bookstore.db"), &gorm.Config{})
+
+	if err != nil {
+		fmt.Printf("failed to connect database")
+	}
+
+	// Create tables
+	db.AutoMigrate(&Book{})
+	println("Database and tables created")*/
+
+}
+
+func createBooks() {
+	//	gofakeit create data
+
+	var count int64
+	db.Model(&Book{}).Count(&count)
+	if count > 0 {
+		println("Book already exists")
+		return
+	}
+	var genres = []string{"Fiction", "Non-fiction", "Fantasy", "Sci-Fi", "Romance", "Thriller"}
+
+	for i := 0; i < 200; i++ {
+		book := Book{
+
+			Title:    gofakeit.BookTitle(),
+			Author:   gofakeit.Name(),
+			Genre:    genres[rand.Intn(len(genres))],
+			Quantity: int(uint(rand.Intn(50) + 1)),
+		}
+		db.Create(&book)
+	}
+	println(fmt.Sprintf("Bookstore loaded %d books", 200))
+}
 
 //get books function
 
@@ -15,14 +78,39 @@ func getBooks(c *gin.Context) {
 }
 
 func addBooks(c *gin.Context) {
+	var book Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&book)
+	c.IndentedJSON(http.StatusCreated, book)
 
 }
 
+func deleteBooks(c *gin.Context) {
+	var book Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Delete(&book)
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func main() {
-	loadDatabase()
 	router := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	loadDatabase()
+	createBooks()
 	router.GET("/books", getBooks)
-	router.Run("localhost:8080")
+	//router.GET("/books/:id", getBooks)
+	router.POST("/books", addBooks)
+	router.DELETE("/books/:id", deleteBooks)
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
 	//	get books function
 
 }
